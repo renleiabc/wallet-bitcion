27520838eed15ae4eeead53b69abbd1ee60bddea
renleiabc <421796896@qq.com>
2021-12-17 15:13:31 +0800

    chain

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -74,7 +74,6 @@ export default {
     );
     const words = bip39.generateMnemonic(160);
     console.log(words);
-    // 比特币
     /*  const words = bip39.generateMnemonic(160);
     console.log(words, '++++++++'); */
     // const words1 = bip39.generateMnemonic(160);
